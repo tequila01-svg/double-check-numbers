d017152fbfb016e992db6187c7ab7636a0c2f983
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-16 14:08:56 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+num=int(input("enter number:"))
+if num>50:
+    print("number is greater than 50")
+    if num%2==0:
+        print("and it is even too")
+    else:
+        print("it is odd")
+else:
+        print("it is less than 50")
